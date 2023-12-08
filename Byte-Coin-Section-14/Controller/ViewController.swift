6f50c76a404dcd0058c6db7a32e1f2f63bd29aba
Abdurahman AYDIN <hariysu@gmail.com>
2023-12-08 19:08:30 +0300

    164-Constructs the project and adds .gitignore

---
+++ Byte-Coin-Section-14/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Byte-Coin-Section-14
+//
+//  Created by Abdurahman on 8.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
