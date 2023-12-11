ff52a79f34ef42b76d8aa687b9b68e44287d240d
Abdurahman AYDIN <hariysu@gmail.com>
2023-12-11 18:25:09 +0300

    167-Makes the api request

--- Byte-Coin-Section-14/Model/CoinManager.swift
+++ Byte-Coin-Section-14/Model/CoinManager.swift
@@ -7,16 +7,45 @@
 
 import Foundation
 
+let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String ?? ""
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
+        
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     func getCoinPrice(for currency: String) {
-        print(currency)
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        print(urlString)
+        performRequest(with: urlString)
     }
+    
+    func performRequest(with urlString:String){
+        //  1. Create a Url:
+                if let url = URL(string: urlString) {
+                    // 2. Create a URLSession:
+                    let session = URLSession(configuration: .default)
+                    // 3. Give the session a task:
+                    let task = session.dataTask(with: url) { data, response, error in
+                        if error != nil {
+                            //self.delegate?.didFailWithError(error: error!)
+                            return
+                        }
+                        if let safeData = data {
+                            let dataString = String(data: safeData, encoding: .utf8)
+                            print(dataString!)
+//                            if let weather = self.parseJSON(safeData){
+//                                self.delegate?.didUpdateWeather(self, weather: weather)
+//                            }
+                        }
+                    }
+                    // 4. Start the task:
+                    task.resume()
+                }
+    }
+    
+    
 
     
 }
